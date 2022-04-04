f9692429ee24f628733dec86123d2e3b9343ace8
Olivier Cagnat <oliviercagnat@gmail.com>
2022-04-04 15:59:52 +0900

    push directory

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,56 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?","%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
+      @flats = Flat.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def edit
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat, notice: 'Flat was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to @flat, notice: 'Flat was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flat_url, notice: 'Flat was successfully destroyed.'
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
+  end
+end
